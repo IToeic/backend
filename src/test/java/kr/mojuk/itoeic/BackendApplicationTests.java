9ba95ddee9d0bbb8ae5f8d9cf101b125c8c0928f
seo003 <sy030212@naver.com>
2025-07-01 14:52:35 +0900

    chore: backend 초기 세팅

---
+++ src/test/java/kr/mojuk/itoeic/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mojuk.itoeic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
