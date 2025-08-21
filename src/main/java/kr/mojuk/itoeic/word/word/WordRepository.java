b163bcbcd7eb005c84e60800c03c24f9f2b168ac
sungmin7723 <smyoun115@gmail.com>
2025-08-21 12:43:37 +0900

    fix: 단어팩 모든 단어 조회 메서드 추가

--- src/main/java/kr/mojuk/itoeic/word/word/WordRepository.java
+++ src/main/java/kr/mojuk/itoeic/word/word/WordRepository.java
@@ -2,9 +2,18 @@ package kr.mojuk.itoeic.word.word;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WordRepository extends JpaRepository<Word, Integer> {
 
     // 랜덤으로 n개 가져오는건 서비스에서 처리
     List<Word> findByWordPack_WordpackId(Integer wordpackId);
+   
+    // 여러 wordpack ID에 해당하는 모든 단어를 조회하는 메서드 추가
+    List<Word> findAllByWordPack_WordpackIdIn(List<Integer> wordpackIds);
+    
+    // DB에 접근하여 5개 조회
+    @Query(value = "SELECT w FROM Word w WHERE w.wordPack.wordpackId = :wordpackId ORDER BY FUNCTION('RAND') LIMIT 5")
+    List<Word> findRandom5ByWordPackId(@Param("wordpackId") Integer wordpackId);
 }
