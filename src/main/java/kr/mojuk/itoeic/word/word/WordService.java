ce89570679f9fdc84949a98210da69826c9360b6
sungmin7723 <smyoun115@gmail.com>
2025-08-26 00:04:35 +0900

    fix: 한 단어팩 api 호출 시 그 api내에서 단어 정렬이 렌덤하게 수정

--- src/main/java/kr/mojuk/itoeic/word/word/WordService.java
+++ src/main/java/kr/mojuk/itoeic/word/word/WordService.java
@@ -3,6 +3,7 @@ package kr.mojuk.itoeic.word.word;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,17 +21,19 @@ public class WordService {
                 .collect(Collectors.toList());
     }
 
-    // 특정 단어팩의 모든 단어를 반환합니다.
+    // 특정 단어팩의 모든 단어를 반환합니다. (순서 섞음)
     public List<WordDTO.Response> getAllWordsByPack(Integer wordpackId) {
         List<Word> words = wordRepository.findByWordPack_WordpackId(wordpackId);
+        Collections.shuffle(words); 
         return words.stream()
                 .map(WordDTO.Response::fromEntity)
                 .collect(Collectors.toList());
     }
 
-    // 여러 단어팩 ID를 받아 해당하는 모든 단어를 반환합니다.
+    // 여러 단어팩 ID를 받아 해당하는 모든 단어를 반환합니다. (순서 섞음)
     public List<WordDTO.Response> getAllWordsByPackIds(List<Integer> wordpackIds) {
         List<Word> words = wordRepository.findAllByWordPack_WordpackIdIn(wordpackIds);
+        Collections.shuffle(words); // 이 부분도 일관성을 위해 추가했습니다.
         return words.stream()
                 .map(WordDTO.Response::fromEntity)
                 .collect(Collectors.toList());
