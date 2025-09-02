3489bb726a94c9339cdd03b944226365346f87a5
sungmin7723 <smyoun115@gmail.com>
2025-09-02 11:07:07 +0900

    fix: AWS 키를 파일에서 받아올 수 있도록 수정

--- src/main/java/kr/mojuk/itoeic/word/TTS/TTSConfig.java
+++ src/main/java/kr/mojuk/itoeic/word/TTS/TTSConfig.java
@@ -1,19 +1,30 @@
 package kr.mojuk.itoeic.word.TTS;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.polly.PollyClient;
 
 @Configuration
 public class TTSConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public PollyClient pollyClient() {
+        // @Value로 주입받은 accessKey와 secretKey를 사용합니다.
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
         return PollyClient.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
